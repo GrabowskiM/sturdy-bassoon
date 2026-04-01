ebc86193c7515d831cc7045f23185b84375c3f2b
Michał Grabowski <michal.grabowski@ibexa.co>
2026-04-01 14:34:28 +0200

    fix

--- update.sh
+++ update.sh
@@ -9,12 +9,6 @@ CLONE_DIR="/tmp/${SOURCE_REPO}-$$"
 
 SOURCE_URL="https://github.com/${OWNER}/${SOURCE_REPO}.git"
 
-if [ -n "$GH_PAT" ]; then
-    TARGET_REMOTE="https://x-access-token:${GH_PAT}@github.com/${OWNER}/${TARGET_REPO}.git"
-else
-    TARGET_REMOTE=""
-fi
-
 echo "Klonowanie ${SOURCE_REPO}..."
 git clone "$SOURCE_URL" "$CLONE_DIR"
 
@@ -34,10 +28,5 @@ fi
 
 git add test.txt
 git commit -m "chore: sync test.txt from ${SOURCE_REPO} [skip ci]"
-
-if [ -n "$CI" ] && [ -n "$TARGET_REMOTE" ]; then
-    echo "Pushowanie do main..."
-    git push "$TARGET_REMOTE" HEAD:main
-fi
 
 echo "Gotowe."
